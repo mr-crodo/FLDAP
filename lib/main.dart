6666e5cbf2575dbb40e237fb6b43420cbad12909
Nasib Mammadov <bduti09@gmail.com>
2026-02-07 11:53:56 +0400

    Weather App

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,28 @@ class MyApp extends StatelessWidget {
 
 Widget _buildBody() {
   return SingleChildScrollView(
-    child: Column(children: <Widget>[_headerImage(), _weatherDescription()]),
+    child: Column(
+      children: <Widget>[
+        _headerImage(),
+        SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                _weatherDescription(),
+                Divider(),
+                _temperature(),
+                Divider(),
+                _temperatureForecast(),
+                Divider(),
+                _footerRatings(),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
   );
 }
 
@@ -55,6 +76,64 @@ Widget _weatherDescription() {
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         style: TextStyle(color: Colors.black54),
       ),
+    ],
+  );
+}
+
+Row _temperature() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[Icon(Icons.wb_sunny, color: Colors.yellow)],
+      ),
+      SizedBox(width: 16.0),
+      Column(
+        children: <Widget>[
+          Text(
+            'Murmanskaya Oblast, Murmansk',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 10.0,
+    children: List.generate(8, (int index) {
+      return Chip(
+        label: Text('${index + 20}C', style: TextStyle(fontSize: 15.0)),
+        avatar: Icon(Icons.wb_cloudy, color: Colors.blue.shade300),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4.0),
+          side: BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      );
+    }),
+  );
+}
+
+Widget _footerRatings() {
+  var stars = Row(
+    mainAxisSize: MainAxisSize.min,
+    children: <Widget>[
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.yellow[600]),
+      Icon(Icons.star, size: 15.0, color: Colors.black),
+      Icon(Icons.star, size: 15.0, color: Colors.black),
+    ],
+  );
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget>[
+      Text('Info with openweathermap.org', style: TextStyle(fontSize: 15.0)),
+      stars,
     ],
   );
 }
